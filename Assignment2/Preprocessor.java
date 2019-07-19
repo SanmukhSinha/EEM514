396b3a6828ff8802329d16aa98605728e11a70f8
SanmukhSinha <sanmukh.s@greyorange.com>
2019-07-19 17:11:23 +0530

    Added define constants

--- Assignment2/Preprocessor.java
+++ Assignment2/Preprocessor.java
@@ -5,6 +5,18 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Enumeration;
+/*
+Every time a new file is opened, the following occur in sequence:
+    create a truecopy of the file
+    removeComments
+    removeWhitespaces
+    scan the file :
+        add #includes recursively using writeInclude
+        create #define data table
+        add #define constants using putDefine Const
+*/
 
 public class Preprocessor {
     private static final String PATH = "E:/Coding/Others/Java/EEM514/Assignment2/";
@@ -14,7 +26,6 @@ public class Preprocessor {
 
         BufferedReader reader = null;
         BufferedWriter writer = null;
-
         try {
             File oldFile = new File(PATH + fileName);
             File newFile = new File(PATH + copyFileName);
@@ -42,6 +53,10 @@ public class Preprocessor {
 
     public static void removeWhitespaces(String fileName) {
         /* removes all whitespaces, call after removeComments */
+        /*Logic: 
+            First finds positon of all valid quotes.
+            Then removes whitespaces in not quoted text, copies quoted text as it is
+        */
 
         createCopy(fileName, "temp" + fileName);
 
@@ -57,7 +72,7 @@ public class Preprocessor {
             writer = new BufferedWriter(fileWrite);
 
             String line;
-            boolean quote = false;
+            boolean quote = false;// outside while to include multi-line quotes
 
             while ((line = reader.readLine()) != null) {
                 line = line.trim();
@@ -90,7 +105,6 @@ public class Preprocessor {
                     for (i = 0; i < quotePos.size(); i += 2) {
                         // inside quotes exactly copied
                         newLine = newLine + line.substring(quotePos.get(i), quotePos.get(i + 1) + 1);
-
                         try {
                             // outside quotes whitespaces removed
                             tempLine = line.substring(quotePos.get(i + 1) + 1, quotePos.get(i + 2));
@@ -122,7 +136,14 @@ public class Preprocessor {
     }
 
     public static void removeComments(String fileName) {
-        /* removes all c-style comments, call first foa a file */
+        /* removes all c-style comments, call first for a file */
+        /*Logic:
+            Copies non-comment text using double boolean variable loop.
+            Replaces comment by a space.
+            Comment can't exist in a Quote and vice-versa.
+            Backslash can only be inside a quote.
+        */
+
         createCopy(fileName, "temp" + fileName);
 
         BufferedReader reader = null;
@@ -197,11 +218,184 @@ public class Preprocessor {
         }
     }
 
+    public static void storeDefine(String line, Hashtable<String, String> defineTableConst,
+            Hashtable<String, String> defineTableFunc) {
+        /*Stores all #define constants and macros in hashtables*/
+        /*Logic:
+            Searches for a define, if found then takes the const(or macro)
+            name (uses'('to check if const or macro).
+            Then takes value till a ' ' for const
+            and tales value till endofline for macro.
+        */
+        int i = line.indexOf("define") + 7;
+        char chr;
+        String str = "", value = "";
+
+        while ((chr = line.charAt(i)) != ' ') {
+            str = str + chr;
+            i++;
+        }
+        i++;
+
+        if (str.indexOf('(') == -1) {
+            while (i < line.length() && (chr = line.charAt(i)) != ' ') {
+                value = value + chr;
+                i++;
+            }
+
+            defineTableConst.put(str, value);
+        } else {
+            if (str.indexOf(')') == -1) {
+                while ((chr = line.charAt(i)) != ')') {
+                    str = str + chr;
+                    i++;
+                }
+                str = str + chr;
+                str = str.replaceAll("\\s+", "");
+                i++;
+            }
+
+            while (i < line.length()) {
+                chr = line.charAt(i);
+                value = value + chr;
+                i++;
+            }
+            value = value.replaceAll("\\s+", "");
+
+            defineTableFunc.put(str, value);
+        }
+    }
+
+    public static String _replaceDefineConst(String line, Hashtable<String, String> defineTableConst) {
+        /* works with putDefineConst(), do not use seperately */
+        /*Logic:
+            Finds for #define const symbols in non-quote area.
+            If found, replace it with its value and return the 
+            edited line with a space at start.
+            This space tells that puDefineConst should be called again
+            for this line.
+            Thus the same line is called repeatedly until all of its
+            consts are replaced with values.
+        */
+
+        Enumeration<String> keys = defineTableConst.keys();
+        String key, value;
+        int index, keyLen;
+
+        while (keys.hasMoreElements()) {
+            key = keys.nextElement();
+            keyLen = key.length();
+            value = defineTableConst.get(key);
+            index = 0;
+
+            while (index < line.length()) {
+                index = line.indexOf(key, index);
+                if (index != -1) {
+                    if (index > 0 && line.substring(index - 1, index).matches("\\w")) {
+                        index += keyLen;
+                    } else if ((index + keyLen) < (line.length() - 1)
+                            && line.substring(index + keyLen, index + keyLen + 1).matches("\\w")) {
+                        index += keyLen;
+                    } else {
+                        line = line.substring(0, index) + value + line.substring(index + keyLen);
+                        return (" " + line);
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+        return line;
+    }
+
+    public static String putDefineConst(String line, Hashtable<String, String> defineTableConst) {
+        /*Replaces #define constants with their value */
+        /*Logic:
+            Finds #define constants in the non-quoted area.
+            For more info look in _replaceDefine Const().
+        */
+
+        boolean quote = false;
+        int pos = 0, i;
+        String newLine = "", tempLine;
+        ArrayList<Integer> quotePos = new ArrayList<Integer>();// quotePos stores start and end indices of all qoutes
+        line = line.trim();
+
+        while (pos < line.length()) {
+            if (quote) {
+                if (line.charAt(pos) == '\"') {
+                    if (!(pos > 0 && line.charAt(pos - 1) == '\\')) {
+                        quote = false;
+                        quotePos.add(pos);
+                    }
+                }
+            } else {
+                if (line.charAt(pos) == '\"') {
+                    quote = true;
+                    quotePos.add(pos);
+                }
+            }
+            pos++;
+        }
+
+        if (quotePos.size() > 0) {
+
+            tempLine = line.substring(0, quotePos.get(0));
+            tempLine = _replaceDefineConst(tempLine, defineTableConst);
+            if (tempLine.charAt(0) == ' ') {
+                return " "+tempLine.substring(1) + line.substring(quotePos.get(0));
+            } else {
+                newLine = newLine + tempLine;
+            }
+
+            for (i = 0; i < quotePos.size(); i += 2) {
+                newLine = newLine + line.substring(quotePos.get(i), quotePos.get(i + 1) + 1);
+                try {
+                    tempLine = line.substring(quotePos.get(i + 1) + 1, quotePos.get(i + 2));
+                    tempLine = _replaceDefineConst(tempLine, defineTableConst);
+                    if (tempLine.charAt(0) == ' ') {
+                        return " "+newLine + tempLine.substring(1) + line.substring(quotePos.get(i + 2));
+                    } else {
+                        newLine = newLine + tempLine;
+                    }
+                } catch (IndexOutOfBoundsException e) {
+                }
+            }
+
+            if (quotePos.get(quotePos.size() - 1) < line.length() - 1) {
+                tempLine = line.substring(quotePos.get(quotePos.size() - 1) + 1);
+                tempLine = _replaceDefineConst(tempLine, defineTableConst);
+                if (tempLine.charAt(0) == ' ') {
+                    return " "+newLine + tempLine.substring(1);
+                } else {
+                    newLine = newLine + tempLine;
+                }
+            }
+        } else {
+            tempLine = line;
+            tempLine = _replaceDefineConst(tempLine, defineTableConst);
+            if (tempLine.charAt(0) == ' ') {
+                return tempLine;
+            } else {
+                newLine = newLine + tempLine;
+            }
+        }
+        return newLine;
+    }
+
     public static void writeInclude(String str, BufferedWriter writer, ArrayList<String> headerList,
             ArrayList<String> trueFileList) {
         /* To add #include files data */
-        // headerList has names of headerfiles already included to prevent recursion
-
+        /*Logic:
+            Looks for headerFile name in quotes.
+            If accessed for the first time:
+                Adds to trueFileList 
+                Creates a trueCopy
+                Removes Comments
+                Removes Whitespaces
+            HeaderList has names of headerfiles already included to prevent recursion
+        */
+        
         int st = str.indexOf("\"");// finds name of header file enclosed in ""
         int en = str.indexOf("\"", st + 1);
         String headerFile = str.substring(st + 1, en).trim(); // this works for " one.h" but it should not
@@ -226,16 +420,13 @@ public class Preprocessor {
                     line = line.trim();
 
                     if (!line.isEmpty()) {
-                        if (line.charAt(0) == '#') {
+                        if (line.charAt(0) == '#' && (line.indexOf("include") == 1 || line.indexOf("include") == 2)) {
                             // problem: works even if there is garbage b/w include and "..."
                             // but may be more reliable
-                            if (line.indexOf("include") == 1 || line.indexOf("include") == 2) {
-                                // to include header file
-                                writeInclude(line, writer, headerList, trueFileList);
-                            }
+                            writeInclude(line, writer, headerList, trueFileList);
                         } else {
                             writer.write(line + '\n');
-                            System.out.println(line);//
+                            //System.out.println(line);//
                         }
                     }
                 }
@@ -249,6 +440,7 @@ public class Preprocessor {
                 }
             }
         }
+
     }
 
     public static void main(String[] args) {
@@ -261,12 +453,18 @@ public class Preprocessor {
 
         createCopy(fileName, "true" + fileName);
         trueFileList.add(fileName);
+
         removeComments(fileName);
         removeWhitespaces(fileName);
 
+        createCopy(fileName, "temp" + fileName);
+        File tempFile = new File(PATH + "temp" + fileName);
+        File mainFile = new File(PATH + fileName);
+
+        /* Deals with #includes */
         try {
-            FileReader fileRead = new FileReader(PATH + "xyz.c");
-            FileWriter fileWrite = new FileWriter(PATH + "tempxyz.c");
+            FileReader fileRead = new FileReader(tempFile);
+            FileWriter fileWrite = new FileWriter(mainFile);
             reader = new BufferedReader(fileRead);
             writer = new BufferedWriter(fileWrite);
 
@@ -274,17 +472,13 @@ public class Preprocessor {
             while ((line = reader.readLine()) != null) {
                 line = line.trim();
                 if (!line.isEmpty()) {
-
-                    if (line.charAt(0) == '#') {
+                    if (line.charAt(0) == '#' && (line.indexOf("include") == 1 || line.indexOf("include") == 2)) {
                         // for including header files
-                        if (line.indexOf("include") == 1 || line.indexOf("include") == 2) {
-                            ArrayList<String> headerList = new ArrayList<String>();// remove from here to prevent
-                                                                                   // repeated addition
-                            writeInclude(line, writer, headerList, trueFileList);
-                        }
+                        ArrayList<String> headerList = new ArrayList<String>();// remove to prevent repeated addition
+                        writeInclude(line, writer, headerList, trueFileList);
                     } else {
                         writer.write(line + '\n');
-                        System.out.println(line);//
+                        //System.out.println(line);//
                     }
                 }
             }
@@ -294,10 +488,51 @@ public class Preprocessor {
             try {
                 reader.close();
                 writer.close();
+                tempFile.delete();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+
+        /* Deals with #defines */
+        createCopy(fileName, "temp" + fileName);
+        try {
+            FileReader fileRead = new FileReader(tempFile);
+            FileWriter fileWrite = new FileWriter(mainFile);
+            reader = new BufferedReader(fileRead);
+            writer = new BufferedWriter(fileWrite);
+
+            String line;
+            Hashtable<String, String> defineTableConst = new Hashtable<String, String>();
+            Hashtable<String, String> defineTableFunc = new Hashtable<String, String>();
+
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+                if (!line.isEmpty()) {
+                    if (line.charAt(0) == '#' && (line.indexOf("define") == 1 || line.indexOf("define") == 2)) {
+                        storeDefine(line, defineTableConst, defineTableFunc);
+                    } else {
+                        do {
+                            System.out.println(line);
+                            line = putDefineConst(line, defineTableConst);
+                        } while (line.charAt(0) == ' ');
+                        writer.write(line + '\n');
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+                writer.close();
+                tempFile.delete();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        createCopy(fileName, "xyz1.c");
 
         for (int i = 0; i < trueFileList.size(); i++) {// recreates the original files
             String name = trueFileList.get(i);
